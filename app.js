d00201818b18a1d121b786fc934d6ac93d2e2cfd
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-01-02 13:17:33 +0530

    Submit

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app =express();
 
 app.use(express.static(__dirname));
 app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname + '/main.html'));
 })
-
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+});
 module.exports = app;
--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const app =require('./app.js');
 app.listen('3000',()=>{
   console.log("Listening on port 3000");
 })
+module.exports = app;
